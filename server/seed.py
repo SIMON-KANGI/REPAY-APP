b5046a917617d724b5536f29432af55537762805
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-03 19:32:37 +0300

    seed

--- server/seed.py
+++ server/seed.py
@@ -1,37 +1,41 @@
 from app import app
 from models import User, Account, Category
-
 from config import db
 
 with app.app_context():
+    print("Dropping all tables...")
     db.drop_all()
+    print("Creating all tables...")
     db.create_all()
     
-    
-    users=[]
-    
-    users.append(User(username='Simon Mwangi',email='simon.mwangi@repay.com',password="7889Kangi",account_type='personal'))
+    print("Adding users...")
+    users = [
+        User(username='Simon Mwangi', email='simon.mwangi@repay.com', password="7889Kangi", account_type='personal'),
+        User(username='Ngethe Mwas', email='simonmwangi@gmail.com', password="7889Kangi", account_type='personal')
+    ]
     for user in users:
         db.session.add(user)
-        db.session.commit()
-        
-    accounts=[]
+    db.session.commit()
     
-    accounts.append(Account(number=123456789,password="7889Kangi",balance=10000,category_id=1, user_id=1))
+    print("Adding accounts...")
+    accounts = [
+        Account(number='123456789', password="7889Kangi", balance=10000, category_id=1, user_id=1)
+    ]
     for account in accounts:
         db.session.add(account)
-        db.session.commit()
-        
-    categories=[]
+    db.session.commit()
     
-    categories.append(Category(name='Equity'))
-    categories.append(Category(name='Co-operative'))
-    categories.append(Category(name='KCB'))
-    categories.append(Category(name='I&M'))
-    categories.append(Category(name='Stanbic'))
-    categories.append(Category(name='Family'))
+    print("Adding categories...")
+    categories = [
+        Category(name='Equity'),
+        Category(name='Co-operative'),
+        Category(name='KCB'),
+        Category(name='I&M'),
+        Category(name='Stanbic'),
+        Category(name='Family')
+    ]
     for category in categories:
         db.session.add(category)
-        db.session.commit()
+    db.session.commit()
     
-    
+    print("Setup complete.")
