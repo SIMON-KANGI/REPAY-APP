90ad5d72f822dd2fc2726b97bb4731b60d746371
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-05 06:31:42 +0300

    models

--- server/models.py
+++ server/models.py
@@ -8,6 +8,7 @@ class User(db.Model, SerializerMixin):
     serialize_only=('id','username','email','role','account_type')
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
+    profile = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     _password = db.Column(db.String(255), nullable=False)  # Changed column name to _password
     is_active = db.Column(db.Boolean, default=True)
@@ -41,7 +42,7 @@ class User(db.Model, SerializerMixin):
     def password(self, plain_password_text):
         self._password = bcrypt.generate_password_hash(plain_password_text).decode('utf-8')
     
-    def check_password_hash(self, password):
+    def check_password(self, password):
         return bcrypt.check_password_hash(self._password, password)
 
     def to_dict(self):
@@ -51,16 +52,16 @@ class User(db.Model, SerializerMixin):
             'email': self.email,
             'role': self.role,
             'account_type': self.account_type,
-            'accounts':[{'id':account.id, "number": account.number, 'balance': account.balance, 'category':account.category}for account in self.accounts]
+            'accounts':[{'id':account.id, "number": account.number, 'balance': account.balance}for account in self.accounts]
         }
 class Account(db.Model, SerializerMixin):
     __tablename__ = 'accounts'
     serialize_rule=()
-    serialize_only=('id','number','balance','category_id','user_id', 'category')
+    serialize_only=('id','number','balance','category_id','user_id')
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer, nullable=False)
     _password = db.Column(db.String(255), nullable=False)  # Changed column name to _password
-    balance = db.Column(db.Integer, nullable=False)
+    balance = db.Column(db.Integer, default=50000, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     users = db.relationship('User', back_populates="accounts")
@@ -94,7 +95,7 @@ class Account(db.Model, SerializerMixin):
             'balance': self.balance,
             'category_id': self.category_id,
             'user_id': self.user_id,
-            'category':[{'id':category.id, 'name':category.name} for category in self.category]
+            
         }
         
 
