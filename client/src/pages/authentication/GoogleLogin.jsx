86bc64e23ac72d09e5118eb1accd7feab936cfc6
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-07 16:25:13 +0300

    google

--- client/src/pages/authentication/GoogleLogin.jsx
+++ client/src/pages/authentication/GoogleLogin.jsx
@@ -5,47 +5,79 @@ import { setCredentials } from '../../features/auth/Authslice';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-
-const GoogleLogin = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const googleLogin = useGoogleLogin({
-    onSuccess: async (response) => {
-      try {
-        const { access_token } = response;
-        const res = await axios.post('http://127.0.0.1:5555/login/authorized', { access_token });
-
-        const { data } = res;
-        const { access_token: backendAccessToken, refresh_token, user } = data;
-
-        localStorage.setItem('access', backendAccessToken);
-        localStorage.setItem('refresh', refresh_token);
-
-        dispatch(setCredentials({ accessToken: backendAccessToken, user }));
-
-        toast.success(`Logged in as ${user.username}`, { position: 'top-right' });
-        navigate('/', { replace: true });
-      } catch (error) {
+import { GoogleLogin } from '@react-oauth/google';
+import { useLoginMutation } from '../../features/auth/Authapi';
+const LoginGoogle = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [login, { isLoading }] = useLoginMutation();
+    const googleLogin = useGoogleLogin({
+      onSuccess: async (response) => {
+        try {
+          const { access_token } = response;
+          const res = await axios.post('http://127.0.0.1:5555/login/authorized', { access_token });
+  
+          const { data } = res;
+          const { access_token: backendAccessToken, refresh_token, user } = data;
+  
+          localStorage.setItem('access', backendAccessToken);
+          localStorage.setItem('refresh', refresh_token);
+  
+          dispatch(setCredentials({ accessToken: backendAccessToken, user }));
+  
+          toast.success(`Logged in as ${user.username}`, { position: 'top-right' });
+          navigate('/', { replace: true });
+        } catch (error) {
+          toast.error('Login failed. Please try again.');
+        }
+      },
+      onError: () => {
         toast.error('Login failed. Please try again.');
-      }
-    },
-    onError: () => {
-      toast.error('Login failed. Please try again.');
-    },
-  });
-
-  return (
-    <button onClick={() => googleLogin()}>
-      Login with Google
-    </button>
+      },
+    });
+  
+    return (
+        <div>
+            <button onClick={() => googleLogin()}>
+        Sign with Google
+      
+      </button>
+        </div>
+      
+    );
+  };
+  
+  const GoogleAuthProviderWrapper = () => (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <LoginGoogle />
+    </GoogleOAuthProvider>
   );
-};
-
-const GoogleAuthProviderWrapper = () => (
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-    <GoogleLogin />
-  </GoogleOAuthProvider>
-);
+  
 
 export default GoogleAuthProviderWrapper;
+
+// function LoginGoogle(){
+// const onSuccess=res=>{
+//     console.log('Login successful')
+// }
+// const onFailure=res=>{
+//     console.log('Login failed')
+// }
+//     return(
+//         <GoogleOAuthProvider>
+//             <GoogleLogin
+//         clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+//         buttonTitle="Login"
+//         onSuccess={onSuccess}
+//         onFailure={onFailure}
+//         isSignedIn={true}
+//         cookiePolicy={'single_host_origin'}
+
+
+//         />
+//         </GoogleOAuthProvider>
+        
+//     )
+
+// }
+// export default LoginGoogle
