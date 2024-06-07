f8bb91705f4eed0d3870d9d2b273ac455a27040c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-07 16:25:30 +0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 
 import Login from './pages/authentication/Login'
 import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/authentication/SignUp'
 function App() {
   return (
     <>
 <Routes>
   <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<SignUp/>} />
   <Route path="*" element={<h1>404</h1>} />
 </Routes>
     </>
