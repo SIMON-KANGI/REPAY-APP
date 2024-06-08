c5248ea4a00b9c4362bfa413c6f8a62a3d545ef9
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-08 10:13:15 +0300

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,18 @@ import './index.css'
 import { store } from './features/store.jsx'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <Provider store={store}>
-  <BrowserRouter>
-     <App />
-  </BrowserRouter>
-         
+  <Provider store={store}>  
+ 
+  <BrowserRouter> 
+  {/* <ToastContainer> */}
+  <App />  
+  {/* </ToastContainer> */}
+</BrowserRouter>
+ 
   
   </Provider>
    
