d8b541a7d87183736204e8ada7618c63c695af24
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-08 18:38:50 +0300

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,15 +7,18 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <Provider store={store}>  
- 
+ <ChakraProvider>
   <BrowserRouter> 
-  {/* <ToastContainer> */}
+  
   <App />  
-  {/* </ToastContainer> */}
+  
 </BrowserRouter>
+ </ChakraProvider>
+  
  
   
   </Provider>
