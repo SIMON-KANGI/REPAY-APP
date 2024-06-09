f7fe2142201c7caac464916d7d0d05f01a327005
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-09 14:34:48 +0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,16 @@ import { Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import AccountType from './pages/authentication/AccountType'
 import Personal from './pages/authentication/Personal'
+import Business from './pages/authentication/Business'
 function App() {
   return (
     <>
-   <NavBar/>
+   {/* <NavBar/> */}
    <main className='container'>
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/account-personal" element={<Personal/>} />
+      <Route path="/account-business" element={<Business/>} />
       <Route path="/account" element={<AccountType />} />
       <Route path="*" element={<h1>404</h1>} />
     </Routes>
