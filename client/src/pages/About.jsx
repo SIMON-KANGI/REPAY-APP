05cf160fa396fe6f8dde3e277df995c57181b442
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-10 13:18:15 +0300

    about

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
+
+function About() {
+  return (
+    <div>
+    <NavBar/>
+    <section>
+      <h1 className='text-center text-2xl font-bold'>How Repay Works</h1>
+    </section>
+      <Footer/>
+    </div>
+  )
+}
+
+export default About
