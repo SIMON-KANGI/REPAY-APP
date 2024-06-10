b235509230797ea0f00ef30e24454df6e3a70887
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-10 17:18:54 +0300

    account

--- client/src/pages/authentication/AccountType.jsx
+++ client/src/pages/authentication/AccountType.jsx
@@ -35,19 +35,19 @@ function AccountType() {
           <h1 className="text-3xl font-bold text-gray-800">Account Type</h1>
         </div>
         <form onSubmit={handleProceed} className="space-y-4 w-full">
-          <div className="border border-gray-300 p-4 rounded-lg hover:shadow-md transition-shadow">
+          <div className="border border-gray-300 hover:bg-stone-800 hover:text-white p-4 rounded-lg hover:shadow-md transition-shadow">
             <label className="font-bold text-xl flex items-center space-x-2">
               <input type="radio" onChange={handleAccountType} name="accountType" value="personal" checked={accountType === 'personal'} className="form-radio text-blue-600" />
               <span>Personal</span>
             </label>
-            <p className="mt-2 text-gray-600">Create an account to manage your personal transactions.</p>
+            <p className="mt-2 hover:text-stone-200">Create an account to manage your personal transactions.</p>
           </div>
-          <div className="border border-gray-300 p-4 rounded-lg hover:shadow-md transition-shadow">
+          <div className="border border-gray-300 p-4  hover:bg-stone-800 hover:text-white rounded-lg hover:shadow-md transition-shadow">
             <label className="font-bold text-xl flex items-center space-x-2">
               <input type="radio" onChange={handleAccountType} name="accountType" value="business" checked={accountType === 'business'} className="form-radio text-blue-600" />
               <span>Business</span>
             </label>
-            <p className="mt-2 text-gray-600">Create an account to manage your business transactions. Make invoices to send to your clients. Manage all your business accounts and transactions.</p>
+            <p className="mt-2">Create an account to manage your business transactions. Make invoices to send to your clients. Manage all your business accounts and transactions.</p>
           </div>
           <button type="submit" className="w-full bg-green-700 text-white rounded-lg py-3 hover:bg-green-800 transition-colors">
             Proceed
