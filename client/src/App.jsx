8eb1a7b95b371d013de8fc04f484fe1586adf4fe
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-11 22:02:21 +0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,10 @@ import AccountType from './pages/authentication/AccountType'
 import Personal from './pages/authentication/Personal'
 import Business from './pages/authentication/Business'
 import Home from './pages/Home'
-import Dashboard from './pages/Dashboard/Dashboard'
+import Dashboard from './pages/Dashboard/MainDash'
 import Notifications from './pages/Dashboard/Notifications'
 import Invoices from './pages/Dashboard/Invoices'
+import MainDash from './pages/Dashboard/MainDash'
 function App() {
   return (
     <>
@@ -19,11 +20,16 @@ function App() {
      <Route path="/" element={<Home/>} />
      <Route path="/how-it-works" element={<About/>} />
       <Route path="/login" element={<Login />} />
-      <Route path='/my-dashboard' element={<Dashboard />} />
+      <Route path='/my-dashboard' element={<MainDash />}>
+        <Route path="dashboard" element={<Dashboard/>} />
+        <Route path="notifications" element={<Notifications/>} />
+        <Route path="invoices" element={<Invoices/>} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Route>
       <Route path="/account-personal" element={<Personal/>} />
       <Route path="/account-business" element={<Business/>} />
       <Route path="/account" element={<AccountType />} />
-      <Route path={'/notifications'} element={<Notifications/>} />
+      <Route path={'/my-dashboard/notifications'} element={<Notifications/>} />
       <Route path={'/invoices'} element={<Invoices/>} />
       <Route path="*" element={<h1>404</h1>} />
     </Routes>
