6598a77bc4cfa82dae42d7f5cc977203bfc4a5cb
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-21 17:42:51 +0300

    notifications

--- client/src/pages/Dashboard/Notifications.jsx
+++ client/src/pages/Dashboard/Notifications.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import SideBar from './SideBar'
+import TopNav from './TopNav'
 function Notifications() {
   return (
     <div className='flex'>
@@ -8,7 +9,7 @@ function Notifications() {
   <section className='w-full'>
       <TopNav/>
       <div>
-<Dashbord/>
+<h1 className='text-center text-2xl'>Notifications</h1>
       </div>
     </section>
   </div>
