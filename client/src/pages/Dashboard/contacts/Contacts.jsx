daa27ad30f8f1a1d243f6db0d116215a531cdd6b
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-21 17:41:40 +0300

    contacts

--- client/src/pages/Dashboard/contacts/Contacts.jsx
+++ client/src/pages/Dashboard/contacts/Contacts.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
+import React,{useCallback, useEffect,useState} from 'react'
 import CreateContacts from './createContacts'
 
 import TopNav from '../TopNav'
 import SideBar from '../SideBar'
-
+import axios from 'axios'
+import { selectUserData } from '../../../features/auth/Authslice'
+import { useSelector } from'react-redux'
+import ContactList from './ContactList'
 function Contacts() {
+  const user=useSelector(selectUserData)
+  const [formData, setFormData]= useState(
+    {
+      name:'',
+      phone:'',
+      email:'',
+      account:'',
+      user_id:user.id
+    }
+  )
+  const [loading, setLoading] = useState(false);
+
+  
+  const handleChange = useCallback((event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }, []);
+  const handleSubmit=useCallback((event)=>{
+event.preventDefault();
+    setLoading(true);
+    try{
+          axios.post('http://127.0.0.1:5555/contacts', formData)
+          .then(res=>{
+            console.log(res.data);
+            setLoading(false);
+          })
+    }catch(e){
+      console.error(e);
+      setLoading(false);
+      
+    }
+
+
+  }, [formData])
+
   return (
     <div className='flex'>
     <SideBar/>
-    <section>
-        <TopNav/>
-        <div>
-           <CreateContacts/> 
-        </div>
-    </section>
+    <section className='w-full'>
+      <TopNav/>
+      <CreateContacts handleSubmit={handleSubmit} handleChange={handleChange} isLoading={loading}/>
+      <h1 className='text-center text-3xl text-green-400 font-bold'>My Contacts</h1>
+  <ContactList/>
+      </section>
+     
       
     </div>
   )
