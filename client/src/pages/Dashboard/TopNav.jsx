6ee664423f4fdc6d459ffe489c50efc80c6ae54e
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-22 14:00:19 +0300

    topnav

--- client/src/pages/Dashboard/TopNav.jsx
+++ client/src/pages/Dashboard/TopNav.jsx
@@ -2,13 +2,13 @@ import { Tooltip } from '@chakra-ui/react';
 import React, { useState, useEffect } from 'react';
 import { IoNotifications } from "react-icons/io5";
 import { Link } from 'react-router-dom';
-import { selectUserData } from '../../features/auth/Authslice';
 import { useSelector } from 'react-redux';
-
+import { selectUserData } from '../../features/auth/Authslice';
+import useFetch from '../../hooks/UseFetch';
 function TopNav() {
     const user = useSelector(selectUserData);
     const [currentTime, setCurrentTime] = useState(new Date());
-    const notifications = user.notifications;
+    const { data:notifications, loading, error } = useFetch('http://127.0.0.1:5555/notifications');
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -23,8 +23,10 @@ function TopNav() {
         const hours = date.getHours();
         const minutes = date.getMinutes();
         const seconds = date.getSeconds();
-        return `${today.toString().slice(0, 17)}-${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        return `${today.slice(0, 16)}-${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     };
+
+    const filteredNotifications = notifications?.filter(notification => notification.user_id === user.id);
 
     return (
         <div className='w-full flex justify-between items-center h-fit p-4 bg-stone-200 shadow-md'>
@@ -34,16 +36,16 @@ function TopNav() {
                 <ul>
                     <div className='flex items-center'>
                         <Tooltip label='Notifications'>
-                        <div className='notifications'>
-        <Link className='relative flex' to='/cart'>
-    <IoNotifications fontSize='1.5rem' />
-    <span className='rounded-full bg-rose-800 p-1 w-5 h-5 flex items-center justify-center text-white'>
-        {notifications.length}
-    </span>
-</Link>
-
-
-        </div>
+                            <div className='notifications'>
+                                <Link className='relative flex' to='/notifications'>
+                                    <IoNotifications fontSize='1.5rem' />
+                                    {filteredNotifications?.length > 0 && (
+                                        <span className='rounded-full bg-rose-800 p-1 w-5 h-5 flex items-center justify-center text-white'>
+                                            {filteredNotifications.length}
+                                        </span>
+                                    )}
+                                </Link>
+                            </div>
                         </Tooltip>
                         <div className='w-10 h-10 border-4 mx-4 border-stone-300 rounded-full overflow-hidden'>
                             <img src={user.profile} alt={user.username} className='rounded-full w-10 h-10' />
