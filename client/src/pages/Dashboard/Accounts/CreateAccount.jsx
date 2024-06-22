75f890c2a399b136878637a194f84c39c736a325
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-22 13:59:59 +0300

    create account

--- client/src/pages/Dashboard/Accounts/CreateAccount.jsx
+++ client/src/pages/Dashboard/Accounts/CreateAccount.jsx
@@ -1,26 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Modal, ModalBody, ModalContent, Tooltip, ModalFooter, ModalCloseButton, useDisclosure, ModalOverlay, ModalHeader } from '@chakra-ui/react';
 import { selectUserData } from '../../../features/auth/Authslice';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { IoMdAdd } from "react-icons/io";
+import useFetch from '../../../hooks/UseFetch';
 
 function CreateAccount() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const user = useSelector(selectUserData);
-  const [categories, setCategories] = useState([]);
+  const { data: categories, loading, error } = useFetch('http://127.0.0.1:5555/categories');
   const [formData, setFormData] = useState({
     category: '',
     accountNumber: '',
     password: '',
     user_id: user.id
   });
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5555/categories')
-      .then(res => setCategories(res.data))
-      .catch(err => console.error(err));
-  }, []);
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -35,7 +30,7 @@ function CreateAccount() {
         onClose();
       })
       .catch(err => console.error(err));
-      console.log(formData)
+    console.log(formData);
   }
 
   return (
@@ -55,7 +50,7 @@ function CreateAccount() {
                 <label className="mb-2 font-medium">Account Category</label>
                 <select onChange={handleChange} value={formData.category} className="p-2 border border-gray-300 rounded-md" name="category">
                   <option value="">Select Bank</option>
-                  {categories.map(category => (
+                  {categories?.map(category => (
                     <option key={category.id} value={category.id}>{category.name}</option>
                   ))}
                 </select>
@@ -66,7 +61,7 @@ function CreateAccount() {
               </div>
               <div className="flex flex-col">
                 <label className="mb-2 font-medium">Account Password</label>
-                <input type="password" onChange={handleChange} name="password" value={formData.password} className="p-2 border border-gray-300 rounded-md" placeholder="Account password" />
+                <input type="password" onChange={handleChange} name="password" value={formData.password} className="p-2 border border-gray-300 rounded-md" placeholder="Account Password" />
               </div>
               <div>
                 <button type="submit" className="p-2 mt-3 w-full bg-green-900 text-white rounded-md shadow-md hover:bg-green-600 transition-colors duration-300">
