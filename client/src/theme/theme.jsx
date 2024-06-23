964f324f8bf27df214ae97907659ec38724d6022
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-23 11:56:18 +0300

    theme

--- client/src/theme/theme.jsx
+++ client/src/theme/theme.jsx
@@ -27,6 +27,19 @@ const appTheme = extendTheme({
         800: '#9f1239',
         900: '#881337',
       },
+     green: {
+        50: '#f0fdf4',
+        100: '#dcfce7',
+        200: '#bbf7d0',
+        300: '#86efac',
+        400: '#4ade80',
+        500: '#22c55e',
+        600: '#16a34a',
+        700: '#15803d',
+        800: '#166534',
+        900: '#14532d',
+}
+
   },
   fonts: {
     heading: `'Inter', sans-serif`,
