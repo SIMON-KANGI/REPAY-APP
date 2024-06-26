a92239a13d67f65aee5fd0cafdcd33bbe1e186df
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-26 12:24:57 +0300

    serach invoice

--- client/src/pages/Dashboard/invoice/SearchInvoice.jsx
+++ client/src/pages/Dashboard/invoice/SearchInvoice.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function SearchInvoice() {
+function SearchInvoice({value,handleChange}) {
   return (
     <div className='float-end'>
-      <form className='float-end mx-4'>
-        <input type='text' placeholder='Search Invoice' className='p-2 w-60 rounded-md'/>
+      <div className='float-end mx-4'>
+        <input type='text' value={value} onChange={handleChange} placeholder='Search Invoice' className='p-2 w-60 rounded-md'/>
         <button type='submit' className='rounded-md bg-blue-700 text-white px-6 py-2'>Search</button>
-      </form>
+      </div>
     </div>
   )
 }
