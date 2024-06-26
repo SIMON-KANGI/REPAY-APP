bbd3e8ed19120fcc785215676d57f40b9121f991
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-06-26 12:25:23 +0300

    filterinvoice

--- client/src/pages/Dashboard/invoice/FilterInvoice.jsx
+++ client/src/pages/Dashboard/invoice/FilterInvoice.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
-function FilterInvoice() {
+function FilterInvoice({handleStatus}) {
+  const [activeStatus, setActiveStatus]= useState(null)
+
+  const handleClick=useCallback((status)=>{
+    if(activeStatus !==status){
+      setActiveStatus(status)
+      handleStatus(status)
+    }
+  },[activeStatus, setActiveStatus])
   return (
-    <div className='flex m-4'>
-      <button className='bg-stone-800 rounded-sm text-slate-200 font-bol px-6 py-2'>View All</button>
-      <button className='bg-green-600  rounded-sm text-slate-200 font-bol px-6 py-2'>Paid</button>
-      <button className='bg-rose-600  rounded-sm text-slate-200 font-bol px-6 py-2'>Pending</button>
+    <div className='flex float-start mx-4'>
+      <button
+      onClick={()=>handleClick('')}
+       className='bg-stone-800 rounded-sm text-slate-200 font-bold px-6 py-2'>View All</button>
+      <button 
+      onClick={()=>handleClick('Paid')}
+      className={`${activeStatus==='Paid'?'bg-green-800 text-slate-200':'bg-transparent text-stone-900' } rounded-sm border-2 border-gray-500  font-bold px-6 py-2`}>Paid</button>
+      <button 
+      onClick={()=>handleClick('Pending')}
+      className={`${activeStatus==='Pending'?'bg-rose-600 text-slate-200':'text-stone-900'}  rounded-sm  border-2 border-gray-500 font-bold px-6 py-2`}>Pending</button>
     </div>
   )
 }
