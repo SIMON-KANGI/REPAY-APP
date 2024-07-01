4dc0ad5a14099627921915ea14e95be71a6496e1
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-01 15:17:13 +0300

    withdraw api

--- client/src/pages/Dashboard/Accounts/Withdraw.jsx
+++ client/src/pages/Dashboard/Accounts/Withdraw.jsx
@@ -37,7 +37,7 @@ function Withdraw() {
 
     if (formData.Fromaccount_name !== formData.Toaccount_name) {
       try {
-        const response = await axios.post('http://127.0.0.1:5555/withdrawal', formData, {
+        const response = await axios.post('https://repay-app.onrender.com/withdrawal', formData, {
           headers: {
             'Content-Type': 'application/json',
           },
