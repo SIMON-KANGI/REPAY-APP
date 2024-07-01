d4b32778e3e15cae724f2d17d84814b8402f08f0
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-01 20:12:50 +0300

    configured database

--- server/config.py
+++ server/config.py
@@ -5,7 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-import secrets
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -19,13 +21,14 @@ def create_app():
     app.config['JWT_SECRET_KEY'] = '1TZTcxUSuWa_D1afcqD4c9soQdR3ogR3BkPT9vZFUxY' # secret key
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 600
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    print("Database URI:", app.config["SQLALCHEMY_DATABASE_URI"])
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
-    jwt = JWTManager(app) # create a JWT token
+    jwt = JWTManager(app)  # create a JWT token
 
     db.init_app(app)
     bcrypt.init_app(app)
-    jwt.init_app(app) # attach a JWT token to the app
+    jwt.init_app(app)  # attach a JWT token to the app
     migrate.init_app(app, db)
     
     return app
