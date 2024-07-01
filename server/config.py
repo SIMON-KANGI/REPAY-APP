9a9a97ccacde6bccdc871d8910904ecbae3df82f
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-01 14:09:57 +0300

    config

--- server/config.py
+++ server/config.py
@@ -6,26 +6,26 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 import secrets
+
 db = SQLAlchemy()
-migrate=Migrate()
-bcrypt=Bcrypt()
-# secret_key=secrets.token_urlsafe(32)
-# print(secret_key)
-# postgresql://repay_user:xvWaPo5xGiGcaGVaOQkYedQqUkkSsweL@dpg-cq17mv6ehbks73ess7g0-a.oregon-postgres.render.com/repay
+migrate = Migrate()
+bcrypt = Bcrypt()
+
 def create_app():
     app = Flask(__name__)
-    CORS(app,supports_credentials=True)
-    app.config['SECRET_KEY']='v3DqpM9dN_IhDaD0zNrPybgwgoF5zQnx0NNPXZfZvVQ'#secret key
-    app.config['JWT_SECRET_KEY']='1TZTcxUSuWa_D1afcqD4c9soQdR3ogR3BkPT9vZFUxY'#secret key
+    CORS(app, supports_credentials=True)
+    
+    app.config['SECRET_KEY'] = 'v3DqpM9dN_IhDaD0zNrPybgwgoF5zQnx0NNPXZfZvVQ' # secret key
+    app.config['JWT_SECRET_KEY'] = '1TZTcxUSuWa_D1afcqD4c9soQdR3ogR3BkPT9vZFUxY' # secret key
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 600
-    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", 'sqlite:///repay.db')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    jwt=JWTManager(app) #create a jwt token
-
+    
+    jwt = JWTManager(app) # create a JWT token
 
     db.init_app(app)
     bcrypt.init_app(app)
-    jwt.init_app(app) #attach a jwt token to the app
-    migrate.init_app(app,db)
+    jwt.init_app(app) # attach a JWT token to the app
+    migrate.init_app(app, db)
     
     return app
