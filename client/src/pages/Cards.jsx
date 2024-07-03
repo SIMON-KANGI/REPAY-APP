32cc216ebed44b3bb5aae547e4dd3f4b05c0b4f0
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-03 17:17:09 +0300

    improved look

--- client/src/pages/Cards.jsx
+++ client/src/pages/Cards.jsx
@@ -59,7 +59,7 @@ function Cards() {
     </Box> 
     <h1 className='text-center text-slate-200 text-4xl'>Manage Your Funds On Repay</h1>
     <Box className='lg:flex justify-center w-full'>
-        <Card background={'transparent'} margin={'2rem'} textColor={'black'} width={'400px'} data-aos="fade-right" className='hover:border hover:shadow-md'>
+        <Card background={'transparent'} margin={'2rem'} textColor={'black'} width={'400px'} data-aos="fade-right" className='hover:border-r-2 hover:border-b-2 hover:border-green-600 hover:shadow-md'>
             <CardBody className=''>
                 <img src="money.png" alt="app" className='lg:rounded-full rounded-md lg:w-80 lg:h-80 w-full h-80' />
                 <h1 className='font-bold text-2xl mt-4'>REPAY Mobile App</h1>
@@ -69,7 +69,7 @@ function Cards() {
             </p>
             </CardBody>
         </Card>
-        <Card background={'transparent'} margin={'2rem'} textColor={'black'} width={'400px'} data-aos="fade-up" className='hover:border hover:shadow-md'>
+        <Card background={'transparent'} margin={'2rem'} textColor={'black'} width={'400px'} data-aos="fade-up" className='hover:border-r-2 hover:border-b-2 hover:border-green-600 hover:shadow-md'>
             <CardBody>
                 <img src="bank.jpg" alt="app" className='lg:rounded-full rounded-md lg:h-80 lg:w-80'/>
                 <h1 className='font-bold text-2xl mt-4'>BANK to BANK<br/>Transfers</h1>
@@ -79,7 +79,7 @@ function Cards() {
             </p>
             </CardBody>
         </Card>
-        <Card background={'transparent'} margin={'2rem'} textColor={'black'} width={'400px'} data-aos="fade-left" className='hover:border hover:shadow-md'>
+        <Card background={'transparent'} margin={'2rem'} textColor={'black'} width={'400px'} data-aos="fade-left" className='hover:border-r-2 hover:border-b-2 hover:border-green-600 hover:shadow-md'>
             <CardBody>
                 <img src="free.jpg" alt="app" className='lg:rounded-full rounded-md lg:w-80 xl:w-72 xl:h-72 lg:h-80'/>
                 <h1 className='font-bold text-2xl mt-4'>REPAY is Free to use</h1>
