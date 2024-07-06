a76eabaf1ca61b824244e490e896e267d4ac5b77
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-06 15:24:43 +0300

    delete account

--- client/src/pages/Details/DeleteAccount.jsx
+++ client/src/pages/Details/DeleteAccount.jsx
@@ -1,19 +1,23 @@
 import { Modal, ModalOverlay, ModalBody, ModalContent, ModalHeader, ModalFooter } from '@chakra-ui/react'
-import React from 'react'
+import {useState} from 'react'
 import { useDisclosure } from '@chakra-ui/react';
-import { selectUserData } from '../../features/auth/Authslice';
-import { useSelector } from 'react-redux';
+import { selectUserData, setCredentials } from '../../features/auth/Authslice';
+import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 function DeleteAccount() {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const [loading, setLoading]= useState(false)
     const user = useSelector(selectUserData);
+    const dispatch=useDispatch()
     function handleDelete(id){
         const response=axios.delete(`https://repay-app.onrender.com/users/${user.id}`)
         if(response.ok){
           console.log('user deleted successfully')
-        
+          setLoading(false);
+          onClose()
+          dispatch(setCredentials({ accessToken:null, username:null, role:null, user:null }));
             }
-        console.log("user not delete")
+        setLoading(true);
     }
     setTimeout(()=>{
         onClose()
@@ -29,7 +33,9 @@ function DeleteAccount() {
           <ModalBody>Are you sure you want to delete your account? This action cannot be undone and all data will be lost!!</ModalBody>
           <ModalFooter className='flex justify-between w-full'>
             <button onClick={onClose} className='bg-stone-900 mx-32 rounded-md  text-white px-8 py-3'>Cancel</button>
-            <button onClick={()=>handleDelete(user.id)} className='bg-rose-900 text-white rounded-md px-8 py-3'>Delete</button> 
+            <button onClick={()=>handleDelete(user.id)} className='bg-rose-900 text-white rounded-md px-8 py-3'>
+            {loading?'Deleting account...':'Delete'}
+            </button> 
           </ModalFooter>
          
         </ModalContent>
