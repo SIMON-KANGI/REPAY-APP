05ac1d086501717b03610268143dc7793944bbde
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-13 12:05:08 +0300

    update stock

--- client/src/pages/Dashboard/Products/productsList.jsx
+++ client/src/pages/Dashboard/Products/productsList.jsx
@@ -1,47 +1,94 @@
-import React,{useState} from 'react'
-import useFetch from '../../../hooks/UseFetch'
+import React, { useMemo, useState } from 'react';
+import axios from 'axios';
 import { FaCheck } from "react-icons/fa";
 import { CgDanger } from "react-icons/cg";
-function ProductsList() {
-  const {data:products}=useFetch('https://repay-app.onrender.com/products')
-const [stockQuantity, setStockQuantity]=useState(20)
+import { Button, Menu, MenuItem, MenuButton, MenuList } from '@chakra-ui/react';
+import { BsChevronBarDown } from 'react-icons/bs';
+import UpdateStock from './UpdateStock';
+import DeleteProduct from './DeleteProduct';
+
+function ProductsList({ products, onClose, onOpen, isOpen }) {
+  const [submitting, setSubmitting] = useState(false);
+  const [stock, setStock] = useState({ id: null, stock: '' });
+  const [productsList, setProductsList] = useState([]);
+
+  useMemo(() => {
+    setProductsList(products);
+  }, [products]);
+
+  const handleChange = (e) => {
+    setStock({ ...stock, [e.target.name]: e.target.value });
+  };
+
+  const handleUpdate = async (updatedProduct) => {
+    try {
+      setSubmitting(true);
+      const result = await axios.patch(`https://repay-app.onrender.com/products/${updatedProduct.id}`, updatedProduct);
+      if (result) {
+        setSubmitting(false);
+        setProductsList(productsList.map(product => product.id === updatedProduct.id ? updatedProduct : product));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='p-4'>
       <table className='min-w-full table-auto'>
         <thead className='p-4'>
           <tr>
-          <th className='px-4 py-2'>ProductId</th>
-          <th className='px-4 py-2'>Product</th>
-            <th lassName='px-4 py-2'>Name</th>
-          
+            <th className='px-4 py-2'>ProductId</th>
+            <th className='px-4 py-2'>Product</th>
+            <th className='px-4 py-2'>Name</th>
             <th className='px-4 py-2'>Price</th>
-             <th className='px-4 py-2'>Category</th>
+            <th className='px-4 py-2'>Category</th>
             <th className='px-4 py-2'>Stock</th>
+            <th className='px-4 py-2'>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {products?.map(product=>(
-            <tr className='bg-white border-b items-center'>
-            <td className='px-4 py-2 text-center'>{product.id}</td>
+          {productsList?.map(product => (
+            <tr key={product?.id} className='bg-white border-b items-center'>
+              <td className='px-4 py-2 text-center'>{product?.id}</td>
               <td className='px-4 py-2 justify-center items-center flex'>
-              <img src={product.profile} className='justify-center rounded-full w-16 h-16' alt={product.name} />
+                <img src={product?.profile} className='justify-center rounded-full w-16 h-16' alt={product?.name} />
               </td>
-              <td className='px-4 py-2 text-center'>{product.name}</td>
-              <td className='px-4 py-2 text-center'>{product.price}</td>
-              <td className='px-4 py-2 text-center'>{product.category}</td>
+              <td className='px-4 py-2 text-center'>{product?.name}</td>
+              <td className='px-4 py-2 text-center'>{product?.price}</td>
+              <td className='px-4 py-2 text-center'>{product?.category}</td>
               <td className='px-4 py-2 flex items-center justify-center '>
-                  {product.stock}
-                  <span className='px-2'>
-                    {product.stock<stockQuantity?<CgDanger color='red'/>:<FaCheck color="green"/>} 
-                  </span> 
-                 
+                {product?.stock}
+                <span className='px-2'>
+                  {product?.stock < 20 ? <CgDanger color='red' /> : <FaCheck color="green" />}
+                </span>
+              </td>
+              <td>
+                <Menu>
+                  <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+                    Choose
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>
+                      <UpdateStock
+                        handleUpdate={handleUpdate}
+                        handleChange={handleChange}
+                        stock={stock}
+                        product={product}
+                      />
+                    </MenuItem>
+                    <MenuItem>
+                      <DeleteProduct onClose={onClose} onOpen={onOpen} isOpen={isOpen} />
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default ProductsList
+export default ProductsList;
