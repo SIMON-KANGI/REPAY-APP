e9c21b4981b38eb7440fc6191039ced9b37fcc2d
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-13 14:17:53 +0300

    delete product

--- client/src/pages/Dashboard/Products/DeleteProduct.jsx
+++ client/src/pages/Dashboard/Products/DeleteProduct.jsx
@@ -1,9 +1,41 @@
+import { Modal, ModalBody,
+  ModalHeader,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalFooter,
+  useDisclosure,Button } from '@chakra-ui/react'
 import React from 'react'
-
-function DeleteProduct() {
+import { MdDelete } from "react-icons/md";
+function DeleteProduct({handleDelete, product}) {
+  const { onClose, onOpen, isOpen } = useDisclosure();
+  // function DeleteProduct(){
+  //   handleDelete(product.id)
+  //   onClose()  
+  // }
   return (
     <div>
-      
+    <button onClick={onOpen} className="items-center flex font-bold justify-center" >
+   <MdDelete/>
+    Delete</button>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Product</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Are you sure you want to delete this product?
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose} variant="outline">
+              Cancel
+            </Button>
+            <Button onClick={handleDelete} variant="solid" colorScheme="red">
+              Delete
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   )
 }
