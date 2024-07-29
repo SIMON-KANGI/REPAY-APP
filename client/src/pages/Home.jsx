1d84d93c4cbcbbd2c8c0fc1449b5d398a0494acc
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-29 22:05:44 +0300

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,12 +17,14 @@ function Home() {
     backgroundImage: "url(/bg.jpg)",
   }}
 >
+<div  className=" bg-gradient-to-r from-stone-950 to-transparent">
   <NavBar
   navStyles="bg-transparent hover:text-black hover:bg-stone-100"/>
-  <div className="flex px-4  lg:flex-row items-center  lg:justify-between">
-    <div className="flex flex-col items-center lg:items-start lg:w-1/2 justify-center text-center lg:text-left">
-      <h1 className="mt-16 leading-loose text-3xl lg:text-5xl text-stone-50 font-bold">
-        MAKE ALL <span className="bg-rose-700 p-2 mb-2 inline-block">YOUR TRANSACTIONS</span> IN ONE APP. RECEIVE, SEND, DEPOSIT
+  <div className="w-screen flex px-4  lg:flex-row items-center  lg:justify-center">
+  
+    <div className="flex  flex-col items-center lg:w-full opacity-85 justify-center text-center lg:text-left">
+      <h1 className="mt-16 leading-loose text-3xl lg:text-4xl text-center text-stone-50 font-bold">
+        MAKE ALL <span className="bg-rose-700 p-2 mb-2 inline-block">YOUR TRANSACTIONS</span> IN ONE APP.<br/> RECEIVE, SEND, DEPOSIT
       </h1>
       <p className="text-2xl leading-loose text-stone-200 mt-4">
         Enjoy easy and instant money transactions. Pay your bills faster and secure <br />
@@ -31,7 +33,7 @@ function Home() {
       <div className="flex flex-col lg:flex-row justify-center items-center lg:items-center my-4 space-y-3 lg:space-y-0 lg:space-x-6">
   <button
     onClick={() => navigate('/account')}
-    className="bg-gradient-to-r from-stone-600 to-stone-950 text-slate-200 font-bold rounded-md lg:rounded-full px-20 py-3 transition-transform transform hover:scale-105 w-full lg:w-auto"
+    className="bg-gradient-to-r from-stone-600 to-rose-600 text-slate-200 font-bold rounded-md lg:rounded-full px-20 py-3 transition-transform transform hover:scale-105 w-full lg:w-auto"
   >
     Start Now
   </button>
@@ -48,7 +50,9 @@ function Home() {
      className="w-1/2 items-center h-1/2 lg:w-1/2 flex justify-center mt-8 lg:mt-0">
       <img src="/person.png" alt="person" className=""height="500px" width="500px"   />
     </div>
-  </div>
+  </div> 
+</div>
+
   <div className='w-screen mt-8 bg-stone-900'>
      <h2 className="lg:text-4xl  text-center   text-xl py-6 text-primary font-semibold text-rose-600">
              <ReactRotatingText items={['Make Transactions', 'Receive Money', 'Withdraw Money','Pay Online']}/>
