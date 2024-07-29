4a26175b964c3a3718857d855dfe76d1932df4dc
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-29 17:26:56 +0300

    global provider

--- client/src/context/GlobalProvider.jsx
+++ client/src/context/GlobalProvider.jsx
@@ -1,66 +1,39 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadUserFromStorage, selectCurrentToken, setCredentials } from '../features/auth/Authslice';
-import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { selectUserData } from '../features/auth/Authslice';
 
 const GlobalContext = createContext();
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
 const GlobalProvider = ({ children }) => {
+  const userData = useSelector(selectUserData);
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const [userData, setUserData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-  const dispatch = useDispatch();
-  const token = useSelector(selectCurrentToken);
+  const [user, setUserData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const storedToken = localStorage.getItem('token');
-
-        if (storedToken) {
-          dispatch(setCredentials({ accessToken: storedToken }));
-        }
-
-        const res = await axios.get('https://repay-app.onrender.com/user/token', {
-          headers: {
-            'Authorization': `Bearer ${storedToken}`
-          }
-        });
-        const data=res.data
-        if (data === storedToken) {
-          dispatch(setCredentials({ accessToken: access_token, username:data.username, role:role, user: data.content }));
-          setLoggedIn(true);
-          setUserData(data.content);
-        } else {
-          setLoggedIn(false);
-          setUserData(null);
-        }
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      } finally {
-        setLoading(false);
+    try {
+      if (userData) {
+        setLoggedIn(true);
+        setUserData(userData);
+      } else {
+        setLoggedIn(false);
+        setUserData(null);
       }
-    };
-
-    fetchUserData();
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem('token', token);
-    } else {
-      localStorage.removeItem('token');
+    } catch (err) {
+      console.error("Error fetching user data:", err);
+    } finally {
+      setIsLoading(false);
     }
-  }, [token]);
+  }, [userData]); // Dependency array includes userData to update when it changes
 
   return (
     <GlobalContext.Provider
       value={{
         isLoggedIn,
+        user,
         setLoggedIn,
-        userData,
         setUserData,
         isLoading,
       }}
