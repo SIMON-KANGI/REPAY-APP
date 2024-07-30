9c53aff3241bc7465587f9442ce322480ce10b5d
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-30 23:07:09 +0300

    partners

--- client/src/components/Partners.jsx
+++ client/src/components/Partners.jsx
@@ -27,9 +27,8 @@ const Partners = () => {
     <div className="partners flex">
       <RotatingLogos duration={60}>
         {logos.map((logo, index) => (
-          <div className="p-8 w-fit h-fit bg-stone-200 m-2 rounded-md flex items-center justify-center" key={index}>
-            <img src={logo} width="100px" height="100px" alt={`Partner ${index + 1}`} className="rounded-full" />
-          </div>
+          
+          <img src={logo} key={index} width="100px" height="100px" alt={`Partner ${index + 1}`} className="rounded-full mx-3" />
         ))}
       </RotatingLogos>
     </div>
