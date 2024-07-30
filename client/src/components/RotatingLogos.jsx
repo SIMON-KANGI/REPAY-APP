6e800c0f59a36d548e1ab22d77cbe79ab23ca0df
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-07-30 19:30:34 +0300

    partners

---
+++ client/src/components/RotatingLogos.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
+
+const RotatingLogos = ({ children, duration }) => {
+  return (
+    <div className="rotating-container overflow-hidden">
+      <motion.div
+        className="scrolling-content flex"
+        animate={{ x: '-100%' }}
+        transition={{ repeat: Infinity, duration, ease: 'linear' }}
+      >
+        {children}
+        {children} {/* Duplicating children for seamless scrolling */}
+      </motion.div>
+    </div>
+  );
+};
+
+RotatingLogos.propTypes = {
+  children: PropTypes.node.isRequired,
+  duration: PropTypes.number,
+};
+
+RotatingLogos.defaultProps = {
+  duration: 10,
+};
+
+export default RotatingLogos;
