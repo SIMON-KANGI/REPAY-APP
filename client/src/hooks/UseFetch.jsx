17b9f35a59357100b3447d354edbbfd0f416c2ea
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-05 14:00:04 +0300

    usefetch

--- client/src/hooks/UseFetch.jsx
+++ client/src/hooks/UseFetch.jsx
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { selectCurrentToken } from '../features/auth/Authslice';
+import { useSelector } from'react-redux';
 function useFetch(url) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+const access_token= useSelector(selectCurrentToken)
   const fetchData = async () => {
     try {
       setLoading(true);
-      const response = await axios.get(url);
+      const response = await axios.get(url,{
+         headers: { Authorization: `Bearer ${access_token}` },
+      }
+       
+      );
       setData(response.data);
     } catch (err) {
       setError(err.message);
