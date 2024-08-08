b115a5601f04e1b15547d735f805c4d4100cb9a9
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-08 12:57:02 +0300

    seed file

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,5 @@
 from app import app
-from models import User, Account, Category, Location
+from models import User, Account, Category, Location, Message, Reply
 from config import db
 
 with app.app_context():
@@ -9,14 +9,14 @@ with app.app_context():
     db.create_all()
     
     print("Adding users...")
-    # users = [
-    #     User(username='Simon Mwangi',profile="img", email='simon.mwangi@repay.com', password="7889Kangi",phone='0797222488', location_id=1, account_type='personal'),
-    #     User(username='Ngethe Mwas',profile="img", email='simonmwangi@gmail.com', password="0797222488kangi", phone='0793452488', location_id=4, account_type='personal'),
-    #     User(username='Ngethe Mwas', profile='img', email='simonmwangikangi@gmail.com', password="7889Kangi", phone='0722447565', location_id=2, account_type='business')
-    # ]
-    # for user in users:
-    #     db.session.add(user)
-    # db.session.commit()
+    users = [
+        User(username='Simon Mwangi',profile="img", email='simon.mwangi@repay.com', password="7889Kangi",phone='0797222488', location_id=1, account_type='personal'),
+        User(username='Ngethe Mwas',profile="img", email='simonmwangi@gmail.com', password="0797222488kangi", phone='0793452488', location_id=4, account_type='personal'),
+        User(username='Ngethe Mwas', profile='img', email='simonmwangikangi@gmail.com', password="7889Kangi", phone='0722447565', location_id=2, account_type='business')
+    ]
+    for user in users:
+        db.session.add(user)
+    db.session.commit()
     
     print("Adding accounts...")
     # accounts = [
@@ -61,4 +61,24 @@ with app.app_context():
         db.session.add(location)
     db.session.commit()
     
+    
+    messages=[
+        Message(body='Hello, how are you?', status="sent", ownerId=1, senderId=2),
+        Message(body='Hello baby.',status="sent", ownerId=2, senderId=1),
+        Message(body='Uko wapi?',status="pending", ownerId=1, senderId=3),
+        Message(body='Ntakam around 10pm.', status="sent", ownerId=3, senderId=1)
+    ]
+    for message in messages:
+        db.session.add(message)
+    db.session.commit()
+        
+    replies=[
+        Reply(body='Hello, im good.',status="sent", ownerId=2, senderId=1, message_id=1),
+        Reply(body='Hello, im good too.', status="sent", ownerId=1, senderId=2, message_id=1),
+        Reply(body='Niko kasa?', ownerId=3, status="pending", senderId=1, message_id=3),
+        Reply(body='Sawa utanishw ukifika.', status="sent", ownerId=1, senderId=3, message_id=4)
+    ]
+    for reply in replies:
+        db.session.add(reply)
+    db.session.commit()
     print("Setup complete.")
