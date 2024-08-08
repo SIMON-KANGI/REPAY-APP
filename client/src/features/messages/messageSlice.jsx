73102a60944cbde9d9d6f54e5d17603f4f74cbcd
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-08 18:53:09 +0300

    message slice

--- client/src/features/messages/messageSlice.jsx
+++ client/src/features/messages/messageSlice.jsx
@@ -1,4 +1,12 @@
-import createSlice from '@reduxjs/toolkit'
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const fetchMessages= createAsyncThunk("messages/fetchMessages", async()=>{
+    const response= await axios.get('http://127.0.0.1:5555/chat/messages')
+ 
+    
+    return response.data
+})
 
 const messageSlice= createSlice({
     name:"messages",
@@ -13,7 +21,28 @@ const messageSlice= createSlice({
         },
         addReply:(state,action)=>{
             const {messageId, reply} = action.payload
-            const message
+            const message=state.messages.find(msg=>msg.id === messageId);
+            if(message){
+                message.replies.push(reply)
+            }
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder
+          .addCase(fetchMessages.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchMessages.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.messages = action.payload;
+          })
+          .addCase(fetchMessages.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+          });
+      }
 })
+
+export const { addMessage, addReply } = messageSlice.actions;
+
+export default messageSlice.reducer;
