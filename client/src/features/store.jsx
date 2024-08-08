1638c1490c2ba615d0cbc3a1b7fa5a48c918df54
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-08 18:53:23 +0300

    store

--- client/src/features/store.jsx
+++ client/src/features/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AuthReducer from './auth/Authslice'
 import { apiSlice } from "./auth/Authapi";
+import messageReducer from "./messages/messageSlice"
 export const store= configureStore({
     reducer:{
         auth:AuthReducer,
+        message:messageReducer,
         api:apiSlice.reducer
     },
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware)
