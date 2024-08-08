0de533c5337f290fdb4734302980596dae62b4fa
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-08 18:52:02 +0300

    messages

--- client/src/pages/message/messageList.jsx
+++ client/src/pages/message/messageList.jsx
@@ -1,29 +1,69 @@
-import React from 'react';
-import { selectUserData } from '../../features/auth/Authslice';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { useMessage } from '../../context/messageContext';
+import { fetchMessages, addMessage } from '../../features/messages/messageSlice';
+import io from 'socket.io-client';
+import { selectUserData } from '../../features/auth/Authslice';
+
+const socket = io('http://127.0.0.1:5555');
 
 function MessageList() {
-  const user = useSelector(selectUserData);
-  const { messages } = useMessage();
+    const dispatch = useDispatch();
+    const user = useSelector(selectUserData);
+    const messages = useSelector(state => state.message?.messages || []);
+    const messageStatus = useSelector(state => state.message?.status);
+    const error = useSelector(state => state.message?.error);
 
-  return (
-    <div className='m-6'>
-      {messages.length > 0 ? (
-        messages.map(message => (
-          <Link to={`/messages/${message.id}`} state={{ message }} key={message.id}>
-            <div className="p-4 bg-slate-500 text-white m-3 rounded-md">
-              <h1 className='text-xl'>{message.receiver || 'Receiver Name'}</h1>
-              <p>{message.body}</p>
-            </div>
-          </Link>
-        ))
-      ) : (
-        <p className='text-center text-gray-600'>No messages available</p>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        const pollMessages = () => {
+            dispatch(fetchMessages());
+        };
+
+        // Initial fetch
+        pollMessages();
+
+        // Poll every 10 seconds
+        const intervalId = setInterval(pollMessages, 10000);
+
+        // Clean up interval on component unmount
+        return () => clearInterval(intervalId);
+    }, [dispatch]);
+
+    useEffect(() => {
+        socket.on('receive_message', (newMessage) => {
+            dispatch(addMessage(newMessage));
+        });
+
+        return () => {
+            socket.off('receive_message');
+        };
+    }, [dispatch]);
+
+    if (messageStatus === 'loading') return <div>Loading...</div>;
+    if (messageStatus === 'failed') return <div>Error: {error}</div>;
+
+    const filteredMessages = messages?.filter(message => {
+        const userMessages = message.senderId === user?.id;
+        const receivedMessages = message.ownerId === user?.id;
+        return userMessages || receivedMessages;
+    });
+
+    return (
+        <div className='m-6'>
+            {filteredMessages?.length > 0 ? (
+                filteredMessages?.map(message => (
+                    <Link to={`/messages/${message.id}`} state={{ message }} key={message.id}>
+                        <div className="p-4 bg-slate-500 text-white m-3 rounded-md">
+                            <h1 className='text-xl'>{message.receiverName || 'Receiver Name'}</h1>
+                            <p>{message.body}</p>
+                        </div>
+                    </Link>
+                ))
+            ) : (
+                <p className='text-center text-gray-600'>No messages available</p>
+            )}
+        </div>
+    );
 }
 
 export default MessageList;
